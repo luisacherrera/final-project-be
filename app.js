1dd1e767762b6c6490dff6191761972ddfcf0cf9
Luis Herrera <la.herrera457@gmail.com>
2018-02-27 16:17:35 +0100

    setting up the back end

---
+++ app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const index = require('./routes/index');
+
+const app = express();
+
+// database setup
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/sample-project', {
+  keepAlive: true,
+  reconnectTries: Number.MAX_VALUE
+});
+
+// middlewares
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({error: 'not found'});
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error('ERROR', req.method, req.path, err);
+
+  if (!res.headersSent) {
+    res.status(500).json({error: 'there was an error'});
+  }
+});
+
+module.exports = app;
